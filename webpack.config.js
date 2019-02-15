adca9f20397b684a7acd6b039150d153b31182bc
fab-zio <fabrizio.musa@yahoo.com>
2019-02-16 00:20:24 +0100

    set webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-    mode: "development",
+    mode: 'development',
     entry: path.resolve(__dirname, 'src', 'app'),
     output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",
-        publicPath: "/"
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/'
     },
     resolve: {
         extensions: ['js', 'jsx']
