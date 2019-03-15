46c2a5fe912e1fd5a889bc631495bfa5102be6e4
fab-zio <fabrizio.musa@yahoo.com>
2019-03-15 17:41:25 +0100

    added combine reducer

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { defaultState } from '../../server/defaultState';
 import { createLogger } from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
@@ -6,10 +6,34 @@ import createSagaMiddleware from 'redux-saga';
 const sagaMiddleware = createSagaMiddleware();
 
 import * as sagas from './sagas.mock';
+import * as mutations from './mutations';
 
-export const store = createStore(function reducer(state = defaultState, action) {
-    return state
-},
+export const store = createStore(combineReducers({
+    tasks(tasks = defaultState.tasks, action) {
+        switch (action.type) {
+            case mutations.CREATE_TASK:
+                // console.log(action);
+                return [...tasks, {
+                    id: action.taskID,
+                    name: "New Task",
+                    group: action.groupID,
+                    ownerID: action.ownerID,
+                    isComplete: false
+
+                }]
+        }
+        return tasks;
+    },
+    comments(comments = defaultState.comments) {
+        return comments
+    },
+    groups(groups = defaultState.groups) {
+        return groups
+    },
+    users(users = defaultState.users) {
+        return users
+    }
+}),
     applyMiddleware(createLogger(), sagaMiddleware)
 );
 
